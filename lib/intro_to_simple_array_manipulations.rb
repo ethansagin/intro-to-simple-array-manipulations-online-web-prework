b843601c80c216d97afc81285e420f4c25f1905f
Ethan <ethansagin@gmail.com>
2019-03-12 20:14:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,10 +1,18 @@
-using_push
+def using_push(array, string)
+  array.push(string)
+end
 
-#using_unshift
+def using_unshift(array, string)
+  array.unshift(string)
+end
 
-#using_pop
+def using_pop(array)
+  array.pop
+end
 
-#pop_with_args
+def pop_with_args
+
+end
 
 #using_shift
 
